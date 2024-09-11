eaef8fe893eb904d388707169ba8b8308ec33788
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-09-11 14:10:33 +0300

    fix payment action

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -3,7 +3,7 @@ import Order from "../models/order.model.js";
 import Gig from "../models/gig.model.js";
 import Stripe from "stripe";
 const intent = async (req, res, next) => {
-  const stripe = new Stripe(process.env.REACT_APP_STRIPE);
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
   const gig = await Gig.findById(req.params.id);
   const paymentIntent = await stripe.paymentIntents.create({
     amount: gig.price * 100,
