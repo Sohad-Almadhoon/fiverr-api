6ca3e68827b80eda01644f2ccc785610e75eba9e
Sohad-Almadhoon <89332592+Sohad-Almadhoon@users.noreply.github.com>
2024-09-17 11:06:26 +0300

    fix issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ credentials: true }));
 const connectToDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
